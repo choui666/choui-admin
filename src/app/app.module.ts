5e9f3bb4b348e8c4def583485571d936fec4f51a
choui666 <choui20016514@gmail.com>
2018-03-12 14:30:11 +0800

    添加本地调试命令

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { environment } from '../environments/environment';
         SharedModule.forRoot(),
         AppRoutingModule,
         BrowserAnimationsModule,
-        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
+        environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
     ],
     providers: [],
     bootstrap: [AppComponent]
